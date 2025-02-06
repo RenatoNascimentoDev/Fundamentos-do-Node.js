018d76b75d00017ff11c8a225ff99ecdcff0c3af
Renato Nascimento <90713455+RenatoNascimentoDev@users.noreply.github.com>
2025-02-06 18:27:58 +0000

    Estrutura da aplicação - Salvando usuários em memória (Headers)

--- src/server.js
+++ src/server.js
@@ -1,6 +1,26 @@
 import http from "node:http";
 
+const users = [];
+
 const server = http.createServer((req, res) => {
+  const { method, url } = (req = req);
+
+  if (method == "GET" && url == "/users") {
+    return res
+      .setHeader("Content-type", "aplication/json")
+      .end(JSON.stringify(users));
+  }
+
+  if (method == "POST" && url == "/users") {
+    users.push({
+      id: 1,
+      name: "John Doe",
+      email: "johndoe@exemple.com",
+    });
+
+    return res.end("Criação de usuário");
+  }
+
   return res.end("Hello World");
 });
 
